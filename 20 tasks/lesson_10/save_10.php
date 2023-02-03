c7385f75c14537a50cbbdfc349e6b8e20b86799b
DESKTOP-DS47177\admin <umalatovumalat780@gmail.com>
2023-02-03 16:58:30 +0300

    lesson_11

--- 20 tasks/lesson_10/save_10.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $text = $_POST['text'];
-
-    $pdo = new PDO("mysql:host=localhost;dbname=tasks","root","");
-    $sql = "INSERT INTO my_table (text) VALUES (:text)";
-    $statement = $pdo->prepare($sql);
-    $statement->execute(['text' => $text]);
-    header('Location: \20 tasks\lesson_10\task_10.php');
