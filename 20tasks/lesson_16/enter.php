4b29cd40a161bdac6f10b3e7bcbfb132a133ee67
DESKTOP-DS47177\admin <umalatovumalat780@gmail.com>
2023-02-27 17:06:19 +0300

    lesson_16

---
+++ 20tasks/lesson_16/enter.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>
+        Подготовительные задания к курсу
+    </title>
+    <meta name="description" content="Chartist.html">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+    <link id="vendorsbundle" rel="stylesheet" media="screen, print" href="../css/vendors.bundle.css">
+    <link id="appbundle" rel="stylesheet" media="screen, print" href="../css/app.bundle.css">
+    <link id="myskin" rel="stylesheet" media="screen, print" href="../css/skins/skin-master.css">
+    <link rel="stylesheet" media="screen, print" href="../css/statistics/chartist/chartist.css">
+    <link rel="stylesheet" media="screen, print" href="../css/miscellaneous/lightgallery/lightgallery.bundle.css">
+    <link rel="stylesheet" media="screen, print" href="../css/fa-solid.css">
+    <link rel="stylesheet" media="screen, print" href="../css/fa-brands.css">
+    <link rel="stylesheet" media="screen, print" href="../css/fa-regular.css">
+</head>
+<body class="mod-bg-1 mod-nav-link ">
+    <form action="save.php" method="post">
+
+        <div class="form-group">
+            <label for="">Вход</label>
+            <input class="form-control" type="text" name="name">
+
+            <div class="form-group mt-2">
+                <button type="submit" class="btn btn-success">Войти</button>
+            </div>
+
+    </form>
+</div>
+</body>
+</html>
+
