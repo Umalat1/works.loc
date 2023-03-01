c91612aadcaa5cbdd6b3e163ffeb00d6359acdc2
DESKTOP-DS47177\admin <umalatovumalat780@gmail.com>
2023-03-01 10:59:34 +0300

    lesson 16

--- 20tasks/lesson_16/save.php
+++ 20tasks/lesson_16/save.php
@@ -1,2 +1,16 @@
 <?php
+    session_start();
+    $name = $_POST['name'];
 
+    $pdo = new PDO('mysql:host=localhost;dbname=tasks;','root','');
+
+    $sql = "SELECT name FROM lesson_16 WHERE 1";
+    $statement = $pdo->prepare($sql);
+    $statement->execute(['name' => $name]);
+    $task = $statement->fetch(PDO::FETCH_ASSOC);
+//    var_dump($task); - выводил $task
+
+    $_SESSION['user'] = $task;
+    var_dump($_SESSION['user']);
+
+    header('location: /20tasks/lesson_16/index.php');
