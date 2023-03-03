f09e5143730976b0a3670a4a0d51453d411db92a
DESKTOP-DS47177\admin <umalatovumalat780@gmail.com>
2023-03-03 14:35:25 +0300

    didn't finish validation

--- my_scripts/php/oop/index.php
+++ my_scripts/php/oop/index.php
@@ -2,6 +2,8 @@
 
 require_once 'Database.php';
 require_once 'Config.php';
+require_once 'Input.php';
+require_once 'Validate.php';
 
 //$users = Database::getInstance()->query("SELECT * FROM users WHERE username IN (?, ?)", ['Umalat', 'Muhammad']);
 
@@ -45,8 +47,57 @@ $GLOBALS["config"] = [
     ]
 ];
 
-$users = Database::getInstance()->query('SELECT * FROM users');
-var_dump($users);
+//$users = Database::getInstance()->query('SELECT * FROM users');
 
+if (Input::exists()) {
+    $validate = new Validate();
+
+    $validation = $validate->check($_POST, [
+        'username' => [
+            'required' => true,
+            'min' => 2,
+            'max' =>15,
+            'unique' => 'users'
+        ],
+        'password' => [
+            'required' => true,
+            'min' => 3
+        ],
+        'password_again' => [
+        'required' => true,
+        'matches' => 'password'
+    ]
+    ]);
+
+    if ($validation->passed()) {
+        echo 'passed';
+    } else {
+        foreach ($validation->errors() as $error) {
+            echo $error . "<br>";
+        }
+    }
+}
+?>
+
+
+<form action="" method="post">
+    <div class="filed">
+        <label for="">Username</label>
+        <input type="text" name="username" value="<?php echo Input::get('username')?>">
+    </div>
+
+    <div class="filed">
+        <label for="">Password</label>
+        <input type="text" name="password">
+    </div>
+
+    <div class="filed">
+        <label for="">Password_again</label>
+        <input type="text" name="password_again">
+    </div>
+
+    <div class="filed">
+        <button type="submit">Submit</button>
+    </div>
 
 
