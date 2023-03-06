aa402fc2b2d981f5feca7b86f31a9a8e751fa546
DESKTOP-DS47177\admin <umalatovumalat780@gmail.com>
2023-03-06 17:04:11 +0300

    login

--- my_scripts/php/oop/classes/User.php
+++ my_scripts/php/oop/classes/User.php
@@ -2,7 +2,7 @@
 
     class User
     {
-        private $db;
+        private $db, $data;
 
         public function __construct()
         {
@@ -13,5 +13,29 @@
         {
             $this->db->insert('users', $fields);
 
+        }
+
+        public  function login($email = null, $password = null) {
+            $user = $this->find($email);
+
+            if ($user) {
+                if (password_verify($password, $this->data()->passwerd)) {
+                    Session::put('user', $this->data()->id);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public function find($email = null) {
+            if ($email) {
+                $this->data = $this->db->get('users', ['email', '=', $email])->first();
+                return true;
+            }
+            return false;
+        }
+
+        public function data() {
+            return $this->data;
         }
     }
--- my_scripts/php/oop/index.php
+++ my_scripts/php/oop/index.php
@@ -1,30 +1,7 @@
 <?php
-//$users = Database::getInstance()->query("SELECT * FROM users WHERE username IN (?, ?)", ['Umalat', 'Muhammad']);
+require_once 'init.php';
 
-//$users = Database::getInstance()->get('users', ['password', '=', "123   "]);
-//if($users->error()) {
-//    echo 'we have on error';
-//} else {
-//    foreach ($users->results() as $user) {
-//        echo $user->username . "<br>";
-//    }
-//}
-
-//Database::getInstance()->delete('users', ['username', '=', "Gilani"]);
-
-//$users = Database::getInstance()->insert('users', [
-//    'username' => 'Gilani',
-//    'password' => "111",
-//]);
-
-//Database::getInstance()->update('users', 11, [
-//    'username' => 'Gilani',
-//    'password' => '333'
-//]);
-
-
-//$users = Database::getInstance()->query('SELECT * FROM users');
-
+var_dump(Session::get('user'));
 
 
 
