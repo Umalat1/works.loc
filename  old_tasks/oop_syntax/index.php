f665c911491962946c2c539f81db16e75cd8a754
User <umalatovumalat780@gmail.com>
2023-11-29 22:14:03 +0300

    $this

---  old_tasks/oop_syntax/index.php
+++  old_tasks/oop_syntax/index.php
@@ -1,10 +1,14 @@
 <?php
 class Person{
-    public $name = "Умалат ";
+    public $name;// Мурад
     public $age = "20 ";
 
     public function  sayHello($name) {
         echo "Hi I am " . $name;
+    }
+
+    public function setName($name){
+        $this->name = $name;// Мурад
     }
 }
 
@@ -12,10 +16,12 @@ class Person{
 $myPerson = new Person;
 $myPerson2 = new Person;
 
-$myPerson->sayHello("Умалат");
-$myPerson->sayHello("Мурад");
-
-//echo $myPerson->name = "Мурад";
+//$myPerson->sayHello("Умалат");
+//$myPerson->sayHello("Мурад");
+$myPerson->setName("Мурад");
+$myPerson2->setName("Умалат");
+echo $myPerson->name;
+echo $myPerson2->name;
 //$myPerson->sayHello();
 //$myPerson->sayHello();
 //$myPerson->sayHello();
